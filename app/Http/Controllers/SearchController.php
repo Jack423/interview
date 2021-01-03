96abc6aaaa4a131184b812b29f8ee03eae540a7a
Jack Butler <guitar423@gmail.com>
2021-01-03 14:25:35 -0600

    Fixed select on search page

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -22,6 +22,59 @@ class SearchController extends Controller
     {
         $inputs = $request->all(['inputCity', 'inputState', 'inputZipCode']);
         $data = new Data;
+        $states = array(
+            'AL'=>'Alabama',
+            'AK'=>'Alaska',
+            'AZ'=>'Arizona',
+            'AR'=>'Arkansas',
+            'CA'=>'California',
+            'CO'=>'Colorado',
+            'CT'=>'Connecticut',
+            'DE'=>'Delaware',
+            'DC'=>'District of Columbia',
+            'FL'=>'Florida',
+            'GA'=>'Georgia',
+            'HI'=>'Hawaii',
+            'ID'=>'Idaho',
+            'IL'=>'Illinois',
+            'IN'=>'Indiana',
+            'IA'=>'Iowa',
+            'KS'=>'Kansas',
+            'KY'=>'Kentucky',
+            'LA'=>'Louisiana',
+            'ME'=>'Maine',
+            'MD'=>'Maryland',
+            'MA'=>'Massachusetts',
+            'MI'=>'Michigan',
+            'MN'=>'Minnesota',
+            'MS'=>'Mississippi',
+            'MO'=>'Missouri',
+            'MT'=>'Montana',
+            'NE'=>'Nebraska',
+            'NV'=>'Nevada',
+            'NH'=>'New Hampshire',
+            'NJ'=>'New Jersey',
+            'NM'=>'New Mexico',
+            'NY'=>'New York',
+            'NC'=>'North Carolina',
+            'ND'=>'North Dakota',
+            'OH'=>'Ohio',
+            'OK'=>'Oklahoma',
+            'OR'=>'Oregon',
+            'PA'=>'Pennsylvania',
+            'RI'=>'Rhode Island',
+            'SC'=>'South Carolina',
+            'SD'=>'South Dakota',
+            'TN'=>'Tennessee',
+            'TX'=>'Texas',
+            'UT'=>'Utah',
+            'VT'=>'Vermont',
+            'VA'=>'Virginia',
+            'WA'=>'Washington',
+            'WV'=>'West Virginia',
+            'WI'=>'Wisconsin',
+            'WY'=>'Wyoming',
+        );
 
         // Keep the text inside the inputs when search is clicked
         $request->flash();
@@ -51,6 +104,7 @@ class SearchController extends Controller
         return view('pages.search', [
             'title' => $this->searchTitle,
             'data' => $data,
+            'states' => $states,
         ]);
     }
 
